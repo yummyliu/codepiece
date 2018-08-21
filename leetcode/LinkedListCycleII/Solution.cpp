bbb85694a610f8e69a7af79ecfdd7172ee7ec623
刘阳明 <liuyangming@liuyangmingdeMacBook-Air.local>
2018-08-22 07:39:07 +0800

    LinkedListCycleII

--- leetcode/LinkedListCycleII/Solution.cpp
+++ leetcode/LinkedListCycleII/Solution.cpp
@@ -1,14 +1,30 @@
 /**
  * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
  */
+#include <iostream>
+#include <map>
+using namespace std;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        
+        if (!head) return NULL;
+ 		map<ListNode*, ListNode*> root;
+		root[head] = head;
+
+		ListNode* nxt = head->next;
+		while (nxt) {
+			if (root.find(nxt) == root.end()) {
+				root[nxt] = root[head];
+			} else {
+				return nxt;
+			}
+            nxt = nxt->next;
+		}
+		return NULL;
     }
 };
