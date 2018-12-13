6dfcede989de84e88423a66f605725f443575606
刘阳明 <liuyangming@liuyangmingdeMacBook-Air.local>
2018-12-13 18:33:55 +0800

    roadmap

--- multiproceee_model/child.c
+++ multiproceee_model/child.c
@@ -1,24 +1,57 @@
 #include "main.h"
+int listen_fd;
+int listen_port;
 
-void doprocessing (void* data) {
-	int sock = *(int*)data;
-	int n;
-	char buffer[256];
-	bzero(buffer,256);
-	n = read(sock,buffer,255);
+struct event ev_channel;
 
-	if (n < 0) {
-		perror("ERROR reading from socket");
+void on_accept(int sockfd, short ev, void *arg){
+
+	log_debug("on_accept: %d, %p\n", ev, arg);
+
+	struct sockaddr_in cli_addr;
+	socklen_t clilen = sizeof(cli_addr);
+
+	int child_accept_sockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+	if (child_accept_sockfd < 0) {
+		log_error("ERROR on accept");
 		exit(1);
 	}
-
-	printf("Here is the message: %s\n",buffer);
-	n = write(sock,"I got your message",18);
-
-	if (n < 0) {
-		perror("ERROR writing to socket");
-		exit(1);
+	if (socketpair(AF_UNIX, SOCK_STREAM, 0, cs[clen].channel) == -1) {
+		log_error("socketpair() failed while spawning \"%s\"", "worker");
 	}
 
+	m_pid = fork();
+	if (m_pid < 0) {
+		log_error("ERROR on fork");
+		exit(1);
+	} else if (m_pid == 0) {
+		m_pid = getpid();
+		ischild = true;
+		my_pos = clen;
+		strncpy(procName,"demo-slave",strlen(procName));
+
+		log_debug("%s %d\n", "this is child: ", m_pid);
+
+		// client_cache = slab_create("client_cache", sizeof(PgSocket), 0, construct_client, USUAL_ALLOC);
+		// TODO new client
+		client_t* curcli;
+
+		event_set(&(curcli->ev_cli), child_accept_sockfd, EV_READ|EV_WRITE|EV_PERSIST, client_handler, curcli);
+		if (event_add(&(curcli->ev_cli), NULL) < 0) {
+			log_error("error add event");
+		}
+		event_set(&ev_channel, cs[my_pos].channel[1], EV_WRITE|EV_PERSIST, channel_handler, NULL);
+		if (event_add(&ev_channel, NULL) < 0) {
+			log_error("error add event");
+		}
+
+		event_del(&ev_accept);
+		close(cs[my_pos].channel[0]);
+		close(listen_fd);
+	} else {
+		close(cs[clen++].channel[1]);
+		clen++;
+		log_debug("fork one child");
+		close(child_accept_sockfd);
+	}
 }
-
--- multiproceee_model/main.c
+++ multiproceee_model/main.c
@@ -14,16 +14,11 @@ child_t cs[MAX_CHILDNUM];
 int clen = 0;
 int my_pos = -1;
 bool ischild=false;
-int pid=-1;
+int m_pid=-1;
 char* procName;
 
-int listen_fd;
-int listen_port;
-
 struct event_base* ebase;
-static struct event ev_accept;
-static struct event ev_read;
-static struct event ev_channel;
+struct event ev_accept;
 
 void usage()
 {
@@ -40,7 +35,6 @@ void demoinit(int argc, char* argv[]) {
 				listen_port = atoi(optarg);
 				break;
 			case 'g':
-				// debug
 				cf_verbose = 1;
 				break;
 			case '?':
@@ -49,7 +43,6 @@ void demoinit(int argc, char* argv[]) {
 				exit(0);
 		}
 	}
-
 }
 
 int main(int argc, char *argv[])
@@ -58,7 +51,6 @@ int main(int argc, char *argv[])
 
 	sig_regist();
 
-	// config listen_socket
 	listen_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (listen_fd < 0) {
 		log_error("ERROR opening socket");
@@ -77,10 +69,8 @@ int main(int argc, char *argv[])
 
 	listen(listen_fd,5);
 
-	// regist event
 	ebase = event_init();
-	/*
-	 *	Pre-2.0 versions of Libevent did not have event_assign() or event_new().
+	/*	Pre-2.0 versions of Libevent did not have event_assign() or event_new().
 	 *	Instead, you had event_set(), which associated the event with the "current" base.
 	 *	If you had more than one base, you needed to remember to call event_base_set()
 	 *	afterwards to make sure that the event was associated with the base you actually wanted to use.
@@ -95,112 +85,9 @@ int main(int argc, char *argv[])
 		log_error("error add event");
 	}
 
-	// loop event
 	if (0 < event_dispatch()) {
 		log_error("error event_dispatch");
 	}
-}
-void on_accept(int sockfd, short ev, void *arg){
-
-	log_debug("on_accept: %d, %p\n", ev, arg);
-
-	struct sockaddr_in cli_addr;
-	socklen_t clilen = sizeof(cli_addr);
-
-	int child_accept_sockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-	if (child_accept_sockfd < 0) {
-		log_error("ERROR on accept");
-		exit(1);
-	}
-	if (socketpair(AF_UNIX, SOCK_STREAM, 0, cs[clen].channel) == -1) {
-		log_error("socketpair() failed while spawning \"%s\"", "worker");
-	}
-
-	pid = fork();
-	if (pid < 0) {
-		log_error("ERROR on fork");
-		exit(1);
-	} else if (pid == 0) {
-		pid = getpid();
-		log_debug("%s %d\n", "this is child: ", pid);
-		ischild = true;
-		my_pos = clen;
-		strncpy(procName,"demo-slave",strlen(procName));
-
-		event_set(&ev_read, child_accept_sockfd, EV_READ|EV_PERSIST, on_read, NULL);
-		if (event_add(&ev_read, NULL) < 0) {
-			log_error("error add event");
-		}
-		event_set(&ev_channel, cs[my_pos].channel[1], EV_WRITE|EV_PERSIST, on_channel, NULL);
-		if (event_add(&ev_channel, NULL) < 0) {
-			log_error("error add event");
-		}
-
-		event_del(&ev_accept);
-		close(cs[my_pos].channel[0]);
-		close(listen_fd);
-	} else {
-		close(cs[clen++].channel[1]);
-		clen++;
-		log_debug("fork one child");
-		close(child_accept_sockfd);
-	}
-}
-
-void on_read(int fd, short ev, void *arg)
-{
-	u_char *buf;
-	int len;
-
-	/* Because we are event based and need to be told when we can
-	 * write, we have to malloc the read buffer and put it on the
-	 * clients write queue. */
-	buf = (u_char*)malloc(BUFLEN);
-	if (buf == NULL)
-		log_error("malloc failed");
-
-	len = read(fd, buf, BUFLEN);
-	if (len == 0) {
-		/* Client disconnected, remove the read event and the
-		 * free the client structure. */
-		log_info("Client disconnected.\n");
-		close(fd);
-		event_del(&ev_read);
-		return;
-	} else if (len < 0) {
-		/* Some other error occurred, close the socket, remove
-		 * the event and free the client structure. */
-		log_debug("Socket failure, disconnecting client: %s",
-				strerror(errno));
-		close(fd);
-		event_del(&ev_read);
-		return;
-	}
-
-	log_debug("%d got msg: %s\n", pid, buf);
-	// /* We can't just write the buffer back as we need to be told
-	//  * when we can write by libevent.  Put the buffer on the
-	//  * client's write queue and schedule a write event. */
-	// bufq = (struct bufferq*)calloc(1, sizeof(struct bufferq));
-	// if (bufq == NULL)
-	// 	err(1, "malloc faild");
-	// bufq->buf = buf;
-	// bufq->len = len;
-	// bufq->offset = 0;
-	// TAILQ_INSERT_TAIL(&client->writeq, bufq, entries);
-
-	// /* Since we now have data that needs to be written back to the
-	//  * client, add a write event. */
-	// if (0 < event_add(&client->ev_write, NULL)) {
-	// 	log_error("Error event add");
-	// }
-}
-
-void on_channel(int sockfd, short ev, void *arg){
-	struct msghdr msg;
-	recvmsg(sockfd, &msg, 0);
-	// TODO
-
 }
 
 
